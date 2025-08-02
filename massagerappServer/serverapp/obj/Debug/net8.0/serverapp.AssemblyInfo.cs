eec08795204677734c7683e474c955303c2b57b2
Slincess <kisacikdevran0@gmail.com>
2025-08-02 04:14:11 +0200

    selfhost ready

--- massagerappServer/serverapp/obj/Debug/net8.0/serverapp.AssemblyInfo.cs
+++ massagerappServer/serverapp/obj/Debug/net8.0/serverapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("serverapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41c32113ad4c865f8f9adcbb7781f06749d8f1ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f38b491fb36bd3413491fcdb4f8af34f3b2353")]
 [assembly: System.Reflection.AssemblyProductAttribute("serverapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("serverapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
